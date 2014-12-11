d175c487737394b0ce9c894e719c57dfc2dd75ca
LIU SHURUI <jackliusr@gmail.com>
2014-12-11 23:59:21 +0800

    web-mode js

--- init.el
+++ init.el
@@ -12,8 +12,11 @@
                       powerline-evil evil-leader evil-paredit evil-visualstar
                       evil-matchit evil-args  evil-nerd-commenter
                       flycheck
-                     projectile multiple-cursors smartparens
+                      projectile multiple-cursors smartparens
+                      async helm
                       simple-httpd js2-mode skewer-mode js2-refactor
+                      web-mode
+                      tern tern-auto-complete
                       ))
 
 (package-initialize)
@@ -67,8 +70,44 @@
 
 (require 'smartparens)
 
+(require 'helm-config)
+
 (setq projectile-enable-caching t)
 (add-hook 'after-init-hook #'global-flycheck-mode)
-(add-hook 'js-mode-hook 'skewer-mode)
-(add-hook 'css-mode-hook 'skewer-css-mode)
-(add-hook 'html-mode-hook 'skewer-html-mode)
+(add-hook 'js-mode-hook (lambda () (flycheck-mode t)))
+;(add-hook 'css-mode-hook 'skewer-css-mode)
+;(add-hook 'html-mode-hook 'skewer-html-mode)
+(add-hook 'js-mode-hook (lambda () (tern-mode t )))
+(eval-after-load 'tern 
+  '(progn 
+     (require 'tern-auto-complete)
+     (tern-ac-setup )))
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode ))
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+ (if (equal web-mode-content-type "jsx")
+    (let ((web-mode-enable-part-face nil))
+      ad-do-it)
+     ad-do-it))
+
+(add-hook 'jsx-mode-hook (lambda () (tern-mode t)))
+(flycheck-define-checker jsxhint-checker
+  "A JSX syntax and style checker based on JSXHint."
+
+  :command ("jsxhint" source)
+  :error-patterns
+  ((error line-start (1+ nonl) ": line " line ", col " column ", " (message) line-end))
+  :modes (web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (equal web-mode-content-type "jsx")
+              ;; enable flycheck
+              (flycheck-select-checker 'jsxhint-checker)
+              (flycheck-mode))))
